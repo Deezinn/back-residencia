97323b4abb1f8d523c5c2f74a6fe4cb7f3d17320
deezinn <developer.andrelz@gmail.com>
2025-06-01 23:15:36 -0300

    feat(fix): finalmente funciona!

--- app/controller/image_controller.py
+++ app/controller/image_controller.py
@@ -4,9 +4,9 @@ from service.image_service import send_image_to_api
 router = APIRouter()
 
 @router.post('/upload')
-async def upload_image(document: UploadFile = File(...),documentClassification: str = Form(...)):
+async def upload_image(image: UploadFile = File(...),documentClassification: str = Form(...)):
    try:
-      response = await send_image_to_api(document,documentClassification)
+      response = await send_image_to_api(image,documentClassification)
       return {"message": "Imagem enviada com sucesso!", "response": response}
    except Exception as e:
       raise HTTPException(status_code=500, detail=str(e))
--- app/service/image_service.py
+++ app/service/image_service.py
@@ -1,5 +1,5 @@
 import base64
-import httpx
+import requests
 from fastapi import UploadFile
 from dotenv import load_dotenv
 import os
@@ -10,8 +10,8 @@ API_TOKEN = os.getenv("API_TOKEN")
 API_URL = os.getenv("API_URL")
 userId = os.getenv('USER_ID')
 
-async def send_image_to_api(file: UploadFile, documentClassification: str):
-   content = await file.read()
+async def send_image_to_api(image: UploadFile, documentClassification: str):
+   content = await image.read()
    encoded_image = base64.b64encode(content).decode('utf-8')
 
    payload = {
@@ -21,11 +21,9 @@ async def send_image_to_api(file: UploadFile, documentClassification: str):
    }
 
    headers = {
-      "Authorization": f"Bearer {API_TOKEN}",
-      "Content-Type": "application/json"
+      "Authorization": API_TOKEN,
    }
 
-   async with httpx.AsyncClient() as client:
-      response = await client.post(API_URL, json=payload, headers=headers)
-      response.raise_for_status()
-      return response.json()
+   response = requests.post(API_URL, json=payload, headers=headers)
+   response.raise_for_status()
+   return response.json()

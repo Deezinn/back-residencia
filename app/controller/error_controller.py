31003a9f8ffc9ff2cbe22bc002b1dd9e2951723c
deezinn <developer.andrelz@gmail.com>
2025-06-02 00:30:09 -0300

    final.

--- app/controller/error_controller.py
+++ app/controller/error_controller.py
@@ -4,11 +4,11 @@ from app.service.error_service import get_error_by_id
 
 router = APIRouter()
 
-@router.get("/documents/{id}")
+@router.get("/error/{id}")
 async def get_document(id: str):
    error = await get_error_by_id(id)
    if not error:
-      raise HTTPException(status_code=404, detail="Documento não encontrado")
+      raise HTTPException(status_code=404, detail="Erro não encontrado")
    error["id"] = str(error["_id"])
    del error["_id"]
    return error
